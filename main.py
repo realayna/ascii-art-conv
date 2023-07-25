6e27b97d46cecf9b68b54bbe6736a26bc010bda8
realayna <ayna1204@gmail.com>
2023-07-25 14:04:26 +0200

    meow

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
 from PIL import Image, ImageDraw, ImageFont
-
 import numpy as np
+from functions import*
 
 
-ASCII_CHARS = "@%#*-+=:. "
-
-def resize_image(img, n_width=100):
-    width, height = img.size
-    ratio=height/float(width)
-    n_height = int(n_width*ratio)
-    resized_image = img.resize((n_width, n_height))
-    return resized_image
-
-def grayify(img):
-    return img.convert("L")
-
-def pixel_to_ascii(img):
-    pixels=img.getdata()
-    ascii_str= ""
-    for pixel_value in pixels:
-        index = pixel_value * len(ASCII_CHARS) // 256
-        ascii_str += ASCII_CHARS[index]
-    return ascii_str
-
-
-
-def main(image_path, new_width=50):
+def main(image_path, new_width=100):
     try:
         image = Image.open(image_path)
     except Exception as e:
@@ -49,4 +27,4 @@ def main(image_path, new_width=50):
 
 if __name__ == "__main__":
     image_path = "stewie.png"  
-    main(image_path, 50)
+    main(image_path)
